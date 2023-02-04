93c3594e6738ba7af70093e0e4c5e4f64a5d05c8
Giorgi <75910445+GeorgeMachaidze@users.noreply.github.com>
2023-02-05 01:13:34 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,87 @@
+var buttonColours = ["red","blue","green","yellow"];
+
+var userClickedPattern = [];
+var gamePattern = [];
+
+
+var started = true;
+
+var level = 0;
+
+//key press function for body
+$("body").keypress(function(){
+  if (started == true){
+  $("h1").text("Level "+level);
+  nextSequence();
+  started = false;
+};
+});
+
+
+//btn click function
+$(".btn").click(function(){
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length -1);
+
+});
+//chacking user answer
+function checkAnswer(currentLevel){
+  if(userClickedPattern[currentLevel] === gamePattern[currentLevel]){
+    console.log("success");
+  if(userClickedPattern.length === gamePattern.length){
+    setTimeout(function(){
+      nextSequence();
+    },100);
+  };
+
+  }else{
+    console.log("wrong");
+    var audio = new Audio('sounds/wrong.mp3');
+    audio.play();
+    $("body").addClass("game-over");
+    setTimeout(function(){
+      $("body").removeClass("game-over");
+    },200);
+    $("h1").text("Game Over, Press Any Key to Restart");
+    startOver();
+  };
+};
+//give user next random sequence increas level
+function nextSequence() {
+
+  userClickedPattern = [];
+
+  level++;
+
+  $("h1").text("Level "+level);
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#"+ randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+};
+//new game function
+function startOver(){
+  level = 0;
+  gamePattern = [];
+  started = true;
+}
+//Button sounds
+function playSound(name){
+  var audio = new Audio('sounds/' + name +'.mp3');
+  audio.play();
+};
+// Button animations
+function animatePress(currentColour){
+
+  $("#"+currentColour).addClass("pressed");
+  setTimeout(function(){
+  $(".btn").removeClass("pressed");
+  },100);
+  };
